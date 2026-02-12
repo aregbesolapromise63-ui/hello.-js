cf6b8871f42a8d08a94fef1852f80b03f3908364
Aregbesola Promise Joshua <aregbesolapromise63@gmail.com>
2026-02-13 00:15:22 +0100

    I modified certain things

--- intro.js
+++ intro.js
@@ -1,13 +1,16 @@
-const name="Promise"
+const username="Promise"
 const age=22
 const birthyear =2009
 const user{
+  const favourite programming language=Javascript
   name:Promise, age:22
   let age=12
 
 
 }
-console.log(name)
+console.log("username:Promise")
+console.log("age:22")
+console.log("favourite programming language:Javascript")
 function greet("Ade")
 return isvalidEmail("@") && (".")
 console.log/greet("Ade")
