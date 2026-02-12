980e1797a1270f87d584e6abcc3477ae9114975b
Aregbesola Promise Joshua <Aregbesola Promise>
2026-02-12 19:25:00 +0100

    Initial commit: Hello world project

---
+++ intro.js
@@ -0,0 +1,18 @@
+const name="Promise"
+const age=22
+const birthyear =2009
+const user{
+  name:Promise, age:22
+  let age=12
+
+
+}
+console.log(name)
+function greet("Ade")
+return isvalidEmail("@") && (".")
+console.log/greet("Ade")
+function validate(){
+  var username=document.getElementsByClassName ("Classname").value
+  console.log(user)
+}
+
